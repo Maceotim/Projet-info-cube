6cf042ddfedfb8fcbb0c86056265b5cba486b4e5
Jules Richard <jules.richard@etu.minesparis.psl.eu>
2026-01-21 14:20:30 +0100

    maj cube.py 1

--- cube.py
+++ cube.py
@@ -7,15 +7,17 @@ import matplotlib.pyplot as plt
 #Conservation du moment :
 # m1v1 + m2v2 = cste
 
-class Cube(self, masse, speed, abscisse):
+class Cube:
 
-    __init__():
-    self.masse = masse
-    self.vitesse = vitesse
-    self.abscisse = abscisse
+    def __init__(self, masse, vitesse, abscisse):
+        self.masse = masse
+        self.vitesse = vitesse
+        self.abscisse = abscisse
 
 
 cube1 = Cube(1, 0, 10)
 cube2 = Cube(2, -1, 200)
 
-energie = 0.5(cube1.masse*cube1.vitesse**2 + cube2.masse*cube2.vitesse**2)
+energie = 0.5*(cube1.masse*cube1.vitesse**2 + cube2.masse*cube2.vitesse**2)
+
+print(np.random.randint(10))
